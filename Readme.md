38fe4617ada94b319caf529481887b955232838e
Katy248 <petrovanton247@gmail.com>
2024-05-06 19:54:28 +0300

    Update

---
+++ Readme.md
@@ -0,0 +1,13 @@
+# Nix home configuration
+
+To setup **nix** use:
+
+```bash
+make setup
+```
+
+To stow configuration use:
+
+```bash
+make stow
+```
--- home.nix
+++ home.nix
@@ -9,7 +9,35 @@
     username = "katy";
     homeDirectory = "/home/katy";
     stateVersion = "23.11";
+    language =
+    let
+      russian = "ru_RU.UTF-8";
+      english = "en_US.UTF-8";
+    in {
+      base = english;
+      # messages = russian;
+      measurement = russian;
+      monetary = russian;
+      # name = russian;
+      numeric = russian;
+      paper = russian;
+      # telephone = russian;
+      time = russian;
+    };
+    sessionVariables = {};
   };
+
+  gtk = {
+    iconTheme = {
+      package = pkgs.papirus-icon-theme;
+      name = "Papirus";
+    };
+    theme = {
+      name = "Adw-gtk3";
+      package = pkgs.adw-gtk3;
+    };
+  };
+  
   programs = {
     home-manager.enable = true;
     git = {
@@ -19,6 +47,10 @@
     };
     helix = {
       enable = true;
+      defaultEditor = true;
+      extraPackages = with pkgs; [
+        # nil marksman omnisharp-roslyn # language servers
+      ];
       settings = {
         theme = "gruvbox";
         editor.cursor-shape = {
@@ -29,12 +61,15 @@
       };
     };
   };
+  services = {
+    syncthing = {
+      enable = true;
+    };
+  };
 
   home.packages = with pkgs; [
-    helix
-    git
     gh
-    # language servers
-    omnisharp-roslyn nil marksman
+    
+    nil marksman omnisharp-roslyn # language servers
   ];
 }
