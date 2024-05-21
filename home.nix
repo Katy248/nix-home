912d4f1bbb9d2f483358dad935cd6648e9609a66
Katy248 <petrovanton247@gmail.com>
2024-05-21 08:47:37 +0300

    Add alacritty configuration

--- home.nix
+++ home.nix
@@ -40,6 +40,52 @@
   
   programs = {
     home-manager.enable = true;
+    alacritty = {
+      enable = true;
+      settings = {
+        shell = "zsh";
+        cursor.style = {
+            blinking = "On";
+            shape = "Beam";
+        };
+        window = {
+          dynamic_title = true;
+          title = "Terminal";
+          padding = { x = 10; y = 10; };
+          opacity = 0.9;
+        };
+        font = {
+          size = 11;
+          normal.family = "JetBrainsMono NL";
+        };
+        colors = {
+          primary = {
+            background = "#282828";
+            foreground = "#ebdbb2";
+          };
+          normal = {
+            black = "#282828";
+            red = "#cc241d";
+            green = "#98971a";
+            yellow = "#d79921";
+            blue = "#458588";
+            magenta = "#b16286";
+            cyan = "#689d6a";
+            white = "#a89984";
+          };
+          bright = {
+            black = "#928374";
+            red = "#fb4934";
+            green = "#b8bb26";
+            yellow = "#fabd2f";
+            blue = "#83a598";
+            magenta = "#d3869b";
+            cyan = "#8ec07c";
+            white = "#ebdbb2";
+          };
+        };
+      };
+    };
     git = {
       enable = true;
       userName = "Katy248";
