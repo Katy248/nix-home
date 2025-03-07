44ccedc25d748ba6d4a564d12c1b232b2e966693
Katy248 <petrovanton247@gmail.com>
2025-03-07 14:40:21 +0300

    changes from notebook.work.katy on Пт 07 мар 2025 14:40:21 MSK

--- programs/git-sync.nix
+++ programs/git-sync.nix
@@ -3,12 +3,12 @@
     enable = true;
     repositories = let
       HOME = config.home.homeDirectory;
-      defaultSyncTimeout = 3600 * 3;
+      defaultSyncInterval= 3600 * 3;
     in {
       ksc = {
         uri = "git@github.com:Katy248/ksc.git";
         path = "${HOME}/src/ksc";
-
+        interval
       };
       neovim = {
         uri = "git@gitlab.com:configs505752/neovim-config.git";
