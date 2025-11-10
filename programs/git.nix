f69c52d8226acc238af38c1a631a546cd71c5182
Katy248 <petrovanton247@gmail.com>
2025-11-10 11:32:08 +0300

    Update conf

--- programs/git.nix
+++ programs/git.nix
@@ -1,9 +1,11 @@
 { ... }: {
   programs.git = {
     enable = true;
-    userName = "Katy248";
-    userEmail = "petrovanton247@gmail.com";
-    extraConfig = { init = { defaultbranch = "main"; }; };
+    settings = {
+      user.ame = "Katy248";
+      user.email = "petrovanton247@gmail.com";
+      init = { defaultbranch = "main"; };
+    };
     ignores = [ ".env" "bin/" "obj/" ];
   };
 }
